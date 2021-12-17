22d45d5d8859d8fe8ed00e40cb5b6b3867b23c27
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-12-17 10:29:09 -0500

    Add a subcommand to generate playlists
    As the CLI is still being fleshed out, all this will do for now is print
    some output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::process;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cue2m3u", about = "Generate playlists for disc-based games.")]
+enum Cli {
+    #[structopt(name = "generate", about = "Generate playlists")]
+    Generate {},
+}
+
+fn dispatch() -> Result<(), ()> {
+    match Cli::from_args() {
+        Cli::Generate {} => println!("generate playlists"),
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    process::exit(match dispatch() {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("error: {:?}", e);
+            1
+        }
+    });
 }
