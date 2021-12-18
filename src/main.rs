8aa0924dad997a9963767e2eb65014ab9acbce98
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-12-18 00:11:39 -0500

    Find cue files in a folder
    This adds a function, `find_cue_files`, that will find any cue files
    contained in the source path. Once they are found, the list will be
    printed out.
    
    `dispatch` will now return `Err<String>` rather than `Err<()>` so that
    `main` has something useful to print to stderr.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::ffi::OsStr;
+use std::fs::read_dir;
+use std::io;
 use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
@@ -19,21 +22,41 @@ enum Cli {
     },
 }
 
-fn dispatch() -> Result<(), ()> {
+fn dispatch() -> Result<(), String> {
     match Cli::from_args() {
         Cli::Generate {
             source,
             recursive,
             overwrite,
-        } => println!(
-            "generate playlists for {:?}{}, {} existing",
-            source,
-            if recursive { " recursively" } else { "" },
-            if overwrite { "overwrite" } else { "skip" }
-        ),
+        } => {
+            println!(
+                "generate playlists for {:?}{}, {} existing",
+                source,
+                if recursive { " recursively" } else { "" },
+                if overwrite { "overwrite" } else { "skip" }
+            );
+            if let Ok(cue_files) = find_cue_files(source) {
+                println!("found {:?}", cue_files);
+            } else {
+                return Err("Error finding cue files".to_owned());
+            }
+        }
     }
 
     Ok(())
+}
+
+fn find_cue_files(source: PathBuf) -> io::Result<Vec<PathBuf>> {
+    let mut cue_files = vec![];
+
+    for path in read_dir(source)? {
+        let path = path?.path();
+        if let Some("cue") = path.extension().and_then(OsStr::to_str) {
+            cue_files.push(path.to_owned());
+        }
+    }
+
+    Ok(cue_files)
 }
 
 fn main() {
