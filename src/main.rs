7492c6928e41bb1d38d54f81916c078717ec3849
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-12-18 00:59:36 -0500

    Find cue files recursively
    Through the introduction of the [walkdir] crate, `find_cue_files` will
    now search for cue files recursively when the `--recursive` option is
    specified.
    
    [walkdir]: https://docs.rs/walkdir/latest/walkdir/

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::ffi::OsStr;
-use std::fs::read_dir;
 use std::io;
 use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
+use walkdir::WalkDir;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "cue2m3u", about = "Generate playlists for disc-based games.")]
@@ -35,7 +34,7 @@ fn dispatch() -> Result<(), String> {
                 if recursive { " recursively" } else { "" },
                 if overwrite { "overwrite" } else { "skip" }
             );
-            if let Ok(cue_files) = find_cue_files(source) {
+            if let Ok(cue_files) = find_cue_files(source, recursive) {
                 println!("found {:?}", cue_files);
             } else {
                 return Err("Error finding cue files".to_owned());
@@ -46,13 +45,17 @@ fn dispatch() -> Result<(), String> {
     Ok(())
 }
 
-fn find_cue_files(source: PathBuf) -> io::Result<Vec<PathBuf>> {
+fn find_cue_files(source: PathBuf, recursive: bool) -> io::Result<Vec<PathBuf>> {
     let mut cue_files = vec![];
 
-    for path in read_dir(source)? {
-        let path = path?.path();
-        if let Some("cue") = path.extension().and_then(OsStr::to_str) {
-            cue_files.push(path.to_owned());
+    let mut walker = WalkDir::new(source).follow_links(true).sort_by_file_name();
+    if !recursive {
+        walker = walker.max_depth(1);
+    }
+    for entry in walker.into_iter().filter_map(|e| e.ok()) {
+        let name = entry.file_name().to_string_lossy();
+        if name.ends_with(".cue") {
+            cue_files.push(entry.path().to_owned());
         }
     }
 
